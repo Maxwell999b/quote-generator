c4a32930e38698a373cac0b71cd3e7f174cbfda0
Abdulrahman Ahmed <bedo150@gmail.com>
2024-09-23 19:46:27 +0300

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "next-themes";
 export default function Home() {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-background text-foreground transition-colors duration-300">
+      <main className="flex min-h-screen flex-col items-center justify-center  bg-background text-foreground transition-colors duration-300">
         <h1 className="text-4xl font-bold mb-8 text-center"> Quote Generator</h1>
         <QuoteGenerator />
       </main>
